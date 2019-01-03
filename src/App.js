aaf818b7423574043eb95703cb01ee2f91fbc3cd
kaan14 <kaankisacik.4@gmail.com>
2019-01-02 21:43:57 -0800

    added About Page

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import About from './pages/About';
+// import Discover from './pages/Discover';
+// import Search from './pages/Search';
+// import Navbar from './components/Navbar';
+// import Wrapper from './components/Wrapper'; 
+
+
+
+const App = () => (
+
+    <Router>
+        <div>
+            
+                    <Route exact path="/" component={About} />
+
+                
+
+
+        </div>
+    </Router>
+
+
+);
+
+
+export default App; 
---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react'; 
+import Hero from '../components/Hero'; 
+
+
+const About = () => (
+
+    <div>
+        <Hero  backgroundImage = "https://images.unsplash.com/photo-1529504426707-d1d7a0dc458a?ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80.com/photos/pIs_JZT49lo">
+            <h1>Motocyle Equipments</h1>
+            <h2>Safety is first for you and your loved ones!!</h2>
+        </Hero>
+    </div>
+
+
+); 
+
+export default About; 
