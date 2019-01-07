7269a7fd5d1904d1d5e8e00d94141dc535923b7c
kaan14 <kaankisacik.4@gmail.com>
2019-01-06 22:06:51 -0800

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import About from './pages/About';
-// import Discover from './pages/Discover';
+import Discover from './pages/Discover';
 // import Search from './pages/Search';
-// import Navbar from './components/Navbar';
+import Navbar from './components/Navbar';
 // import Wrapper from './components/Wrapper';
 
 
@@ -11,9 +11,11 @@ import About from './pages/About';
 const App = () => (
 
     <Router>
+        
         <div>
-    
+        <Navbar/>
                 <Route exact path="/" component={About} />
+                <Route exact path="/discover" component={Discover} />
 
                 
 
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import "./Navbar.css"; 
+
+
+const Navbar = props => (
+    <div>
+        <nav className="" >
+            <Link className="navbar-brand" to="/">
+                Kaan's Kawasaki
+           </Link>
+            <div>
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <Link to="/"
+                            className={
+                                window.location.pathname === "/" ||
+                                    window.location.pathname === "/about"
+                                    ? "nav-link active"
+                                    : "nav -link"
+                            }
+                        >
+                            About
+                    </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/discover"
+                            className={
+                                window.location.pathname === "/discover"
+                                    ? "nav-link active" 
+                                    : "nav-link"
+                            }
+                        >
+                            Discover
+                </Link>
+
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+    </div>
+)
+
+
+export default Navbar;
+
