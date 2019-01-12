ebea38f55e4c35ec20ba8f2de0dff33a39d49061
kaan14 <kaankisacik.4@gmail.com>
2019-01-12 08:34:37 -0800

    added form components

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,8 +1,26 @@
-import React, {components} from 'react'; 
-import './SearchFrom.css'; 
+import React from 'react'; 
+import './SearchForm.css'; 
 
 
 const SearchForm = props => (
+
+    <form className="search">
+    <div className="form-group">
+        <label>Search</label>
+        <input 
+            value={props.search}
+            onChange={props.Function}
+            placeholder="search for any time of a person"
+            id="personType"  
+            className="form-control"      
+        />
+        {/* <datalist id="personTypes">
+        {props.types.map(type=><option value={type} key={type}/>)}
+        </datalist> */}
+
+        <button onClick={props.Functions} type="submit" className="btn">Search</button>
+        </div>
+    </form>
 
 ); 
 
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -8,7 +8,7 @@ class Search extends Component {
 
     render(){
         return(
-
+            <SearchForm/>
         ); 
     }
 
