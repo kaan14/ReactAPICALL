094641b6f5c2beb45cf5ade4906fcaeedcc73641
kaan14 <kaankisacik.4@gmail.com>
2019-01-14 20:43:43 -0800

    added prettier, eslint, styleint

--- src/App.js
+++ src/App.js
@@ -6,25 +6,21 @@ import Search from './pages/Search';
 import Navbar from './components/Navbar';
 import Wrapper from './components/Wrapper';
 
-
-
 const App = () => (
 
-    <Router>
-        
-        <div>
-        <Navbar/>
-        <Wrapper>
-                <Route exact path="/" component={About} />
-                <Route exact path="/discover" component={Discover} />
-                <Route exact path="/search" component={Search}/>
-          
-          </Wrapper>      
-        </div>
-    </Router>
+  <Router>
+    <div>
+      <Navbar />
+      <Wrapper>
+        <Route exact path="/" component={About} />
+        <Route exact path="/discover" component={Discover} />
+        <Route exact path="/search" component={Search} />
+
+      </Wrapper>
+    </div>
+  </Router>
 
 
-);
+); 
 
-
-export default App; 
+export default App;  
